ce817c4e4f2b721fad61b499741e26a31571cd26
sftth <sftth@naver.com>
2021-08-21 17:54:12 +0900

    Initial Commit

---
+++ src/main/java/com/techprimers/springbatchexample1/config/SpringBatchConfig.java
@@ -0,0 +1,74 @@
+package com.techprimers.springbatchexample1.config;
+
+import com.techprimers.springbatchexample1.model.User;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+@EnableBatchProcessing
+public class SpringBatchConfig {
+
+    @Bean
+    public Job job(JobBuilderFactory jobBuilderFactory,
+                   StepBuilderFactory stepBuilderFactory,
+                   ItemReader<User> itemReader,
+                   ItemProcessor<User, User> itemProcessor,
+                   ItemWriter<User> itemWriter) {
+
+        Step step = stepBuilderFactory.get("ETL-file-load")
+                .<User, User>chunk(100)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(itemWriter)
+                .build();
+
+        return jobBuilderFactory.get("ETL-Load")
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<User> itemReader(@Value("${input}")Resource resource) {
+        FlatFileItemReader<User> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(resource);
+        flatFileItemReader.setName("CSV-Reader");
+        flatFileItemReader.setLinesToSkip(1); //읽기 시작 전 제외할 라인 수 설정
+        flatFileItemReader.setLineMapper(lineMapper());
+
+        return flatFileItemReader;
+    }
+
+    @Bean
+    public LineMapper<User> lineMapper() {
+        DefaultLineMapper<User> defaultLineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false); //꼭 라인 상에 토큰 개수가 컬럼 개수와 일치할 필요는 없음
+        lineTokenizer.setNames(new String[]{"id","name","dept","salary"});
+
+        BeanWrapperFieldSetMapper<User> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(User.class);
+
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+        return defaultLineMapper;
+    }
+}
---
+++ src/test/java/com/techprimers/springbatchexample1/SpringBatchExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.springbatchexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
